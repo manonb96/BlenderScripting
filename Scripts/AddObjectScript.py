e4052a6f20fddbde7f922b95f1db4e95608af064
manonb96 <manonblanke_@hotmail.com>
2024-02-17 16:28:06 -0500

    Create additional panels for scaling and special operations

--- Scripts/AddObjectScript.py
+++ Scripts/AddObjectScript.py
@@ -1,26 +1,88 @@
+bl_info = { 
+    "name": "Object Adder",
+    "author": "Manon",
+    "version": (1, 0),
+    "blender": (2, 93, 6),
+    "location": "View3d > Tool",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Add Mesh",
+}
+
 import bpy
 
-class TestPanel(bpy.types.Panel): 
-    bl_label = "Test Panel"
-    bl_idname = "PT_TestPanel"
+class AddObjects(bpy.types.Panel): 
+    bl_label = "AddObjects"
+    bl_idname = "PT_AddObjectsPanel"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'NewCategory'
+    bl_category = 'Add'
     
     def draw(self, context): 
         layout = self.layout
         
         row = layout.row()
-        row.label(text= "Sample Text", icon='CUBE')
+        row.label(text= "Add some new objects", icon='PLUS')
         row = layout.row()
-        row.operator("mesh.primitive_cube_add")
+        row.operator("mesh.primitive_cube_add", icon='CUBE')
+        row.operator("mesh.primitive_torus_add", icon='MESH_TORUS')
+        row = layout.row()
+        layout.scale_y = 1.2
         
+        row.operator("object.text_add")
+
+class PanelA(bpy.types.Panel): 
+    bl_label = "Scaling"
+    bl_idname = "PT_PanelA"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Add'
+    bl_parent_id = 'PT_AddObjectsPanel'
+    bl_options = {'DEFAULT_CLOSED'}
+    
+    def draw(self, context): 
+        layout = self.layout
+        obj = context.object
         
+        row = layout.row()
+        row.label(text= "Select an option to scale your object", icon="OUTLINER_OB_ARMATURE") 
+        row = layout.row() 
+        row.operator("transform.resize")
+        row = layout.row()
+        layout.scale_y = 1.2
+        col = layout.column()
+        
+        col.prop(obj, "scale")
+
+class PanelB(bpy.types.Panel): 
+    bl_label = "Specials"
+    bl_idname = "PT_PanelB"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Add'
+    bl_parent_id = 'PT_AddObjectsPanel'
+    bl_options = {'DEFAULT_CLOSED'}
+    
+    def draw(self, context): 
+        layout = self.layout
+        
+        row = layout.row()
+        row.label(text= "Select a special object", icon="SOLO_ON")
+        row = layout.row()
+        row.operator("object.shade_smooth")      
+        row.operator("object.subdivision_set")
+        row = layout.row()        
+        row.operator("object.modifier_add")         
+    
 def register():
-    bpy.utils.register_class(TestPanel) 
+    bpy.utils.register_class(AddObjects) 
+    bpy.utils.register_class(PanelA) 
+    bpy.utils.register_class(PanelB) 
     
 def unregister():
-    bpy.utils.unregister_class(TestPanel)
+    bpy.utils.unregister_class(AddObjects)
+    bpy.utils.unregister_class(PanelA)
+    bpy.utils.unregister_class(PanelB)
 
 if __name__ == "__main__":
     register()
